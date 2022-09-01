e43f9aaf09a3f361e9db42dcd52d223159e98705
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-01 17:42:39 +0900

    Add: user repository

---
+++ src/main/java/Kitos/TodoApp/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package Kitos.TodoApp.user.domain.repository;
+
+import Kitos.TodoApp.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
