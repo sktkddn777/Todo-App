ce66e18b6db32e79c5a07c9307960565079001e0
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-01 17:40:39 +0900

    Add: todo controller

---
+++ src/main/java/Kitos/TodoApp/todo/controller/viewController/TodoController.java
@@ -0,0 +1,11 @@
+package Kitos.TodoApp.todo.controller.viewController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/todos")
+public class TodoController {
+}
