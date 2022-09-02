71dab5d31641801ffac84d55fd5b89381251852b
a-ryang <chnwu.kim@gmail.com>
2022-09-02 23:39:07 +0900

    Add: 생성, 수정 시간 자동화 JpaAuditing 어노테이션 적용
    * BaseTimeEntity의 createdAt, updatedAt에 자동화를 위한 설정

--- src/main/java/Kitos/TodoApp/TodoAppApplication.java
+++ src/main/java/Kitos/TodoApp/TodoAppApplication.java
@@ -2,7 +2,9 @@ package Kitos.TodoApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing을 활성화 하기 위한 어노테이션
 @SpringBootApplication
 public class TodoAppApplication {
 
