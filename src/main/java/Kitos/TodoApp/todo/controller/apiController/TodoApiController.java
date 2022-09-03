25953098b675f9a746c146f2c1c8e0d908d07a7f
a-ryang <chnwu.kim@gmail.com>
2022-09-03 14:54:58 +0900

    Feat: Todo 생성 API 임시 구현
    * 인증, 유효성 검사 추가 필요

--- src/main/java/Kitos/TodoApp/todo/controller/apiController/TodoApiController.java
+++ src/main/java/Kitos/TodoApp/todo/controller/apiController/TodoApiController.java
@@ -1,6 +1,16 @@
 package Kitos.TodoApp.todo.controller.apiController;
 
+
+import Kitos.TodoApp.todo.dto.request.CreateTodoReqDto;
+import Kitos.TodoApp.todo.dto.response.TodoResDto;
+import Kitos.TodoApp.todo.service.interfaces.TodoService;
+
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/todos")
 public class TodoApiController {
+
+  private final TodoService todoService;
+
+  @PostMapping // TODO: 유효성검사, 인가
+  public ResponseEntity<?> createTodo(@RequestBody CreateTodoReqDto dto) {
+    TodoResDto todo = todoService.createTodo(dto);
+
+    // new ResponseEntity(todo, HttpStatus.CREATED)도 가능
+    return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+  }
 }
--- src/main/java/Kitos/TodoApp/todo/service/TodoServiceImpl.java
+++ src/main/java/Kitos/TodoApp/todo/service/TodoServiceImpl.java
@@ -1,5 +1,9 @@
 package Kitos.TodoApp.todo.service;
 
+import Kitos.TodoApp.todo.domain.Todo;
+import Kitos.TodoApp.todo.domain.repository.TodoRepository;
+import Kitos.TodoApp.todo.dto.request.CreateTodoReqDto;
+import Kitos.TodoApp.todo.dto.response.TodoResDto;
 import Kitos.TodoApp.todo.service.interfaces.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TodoServiceImpl implements TodoService {
+
+  private final TodoRepository todoRepository;
+
+  @Transactional
+  public TodoResDto createTodo(CreateTodoReqDto dto) {
+
+    Todo todo = CreateTodoReqDto.toEntity(dto);
+
+    Todo newTodo = todoRepository.save(todo);
+
+    return new TodoResDto(newTodo);
+  }
 }
