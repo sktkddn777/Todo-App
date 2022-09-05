6e53b788b7dd3d9943770d34a1cf10b18b12e587
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-05 20:51:59 +0900

    Feat: getAll todos

--- src/main/java/Kitos/TodoApp/todo/controller/apiController/TodoApiController.java
+++ src/main/java/Kitos/TodoApp/todo/controller/apiController/TodoApiController.java
@@ -1,18 +1,18 @@
 package Kitos.TodoApp.todo.controller.apiController;
 
 
+import Kitos.TodoApp.todo.domain.Todo;
 import Kitos.TodoApp.todo.dto.request.CreateTodoReqDto;
 import Kitos.TodoApp.todo.dto.response.TodoResDto;
 import Kitos.TodoApp.todo.service.interfaces.TodoService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,5 +27,14 @@ public class TodoApiController {
 
     // new ResponseEntity(todo, HttpStatus.CREATED)도 가능
     return ResponseEntity.status(HttpStatus.CREATED).body(todo);
+  }
+
+  @GetMapping
+  public Page<Todo> getAllTodo(Pageable pageable) {
+//    return StandardResponse.builder()
+//            .message("get All done")
+//            .data(Map.of("todos", todoService.getAllTodo(pageable).orElseThrow()))
+//            .build();
+    return todoService.getAllTodo(pageable).orElseThrow();
   }
 }
--- src/main/java/Kitos/TodoApp/todo/domain/repository/TodoRepository.java
+++ src/main/java/Kitos/TodoApp/todo/domain/repository/TodoRepository.java
@@ -1,9 +1,13 @@
 package Kitos.TodoApp.todo.domain.repository;
 
 import Kitos.TodoApp.todo.domain.Todo;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
 }
