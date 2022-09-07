d9e7727a5f192c39d2652b1c95a7861b606e29d0
chwulll <cksdn623@naver.com>
2022-09-07 21:59:12 +0900

    Add: NotFoundException

---
+++ src/main/java/Kitos/TodoApp/global/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package Kitos.TodoApp.global.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
