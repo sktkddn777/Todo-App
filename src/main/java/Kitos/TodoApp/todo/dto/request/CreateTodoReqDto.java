510f047e1b65a4679ddc6c2635030ef213d9ca5e
한상우 <sangwoha@Sangwoha-MacBook-Air.local>
2022-09-29 14:34:53 +0900

    Feat: TodoCreate 유효성 검사

--- src/main/java/Kitos/TodoApp/todo/dto/request/CreateTodoReqDto.java
+++ src/main/java/Kitos/TodoApp/todo/dto/request/CreateTodoReqDto.java
@@ -6,10 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateTodoReqDto {
+
+  @NotBlank(message = "빈 값이 들어가면 안됩니다")
   private String content;
 
   public static Todo toEntity(CreateTodoReqDto dto){
