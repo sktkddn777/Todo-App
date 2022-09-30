57d31547a3c0d5d3ef5cce6c5a502a42f2e77e75
a-ryang <chnwu.kim@gmail.com>
2022-09-30 14:50:53 +0900

    Feat: 임시 회원가입 API

--- src/main/java/Kitos/TodoApp/user/controller/apiController/UserApiController.java
+++ src/main/java/Kitos/TodoApp/user/controller/apiController/UserApiController.java
@@ -1,11 +1,26 @@
 package Kitos.TodoApp.user.controller.apiController;
 
+
+import Kitos.TodoApp.user.dto.request.SignUpDto;
+import Kitos.TodoApp.user.dto.response.UserResDto;
+import Kitos.TodoApp.user.service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/") //TODO: 유효성검사
+    public ResponseEntity<UserResDto> signUp(@RequestBody SignUpDto dto){
+        UserResDto user = userService.create(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
--- src/main/java/Kitos/TodoApp/user/service/UserServiceImpl.java
+++ src/main/java/Kitos/TodoApp/user/service/UserServiceImpl.java
@@ -1,12 +1,44 @@
 package Kitos.TodoApp.user.service;
 
+import Kitos.TodoApp.global.exception.CustomException;
+import Kitos.TodoApp.user.domain.User;
+import Kitos.TodoApp.user.domain.repository.UserRepository;
+import Kitos.TodoApp.user.dto.request.SignUpDto;
+import Kitos.TodoApp.user.dto.response.UserResDto;
 import Kitos.TodoApp.user.service.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static Kitos.TodoApp.global.exception.ErrorCode.ALREADY_EXISTS_EMAIL;
+import static Kitos.TodoApp.global.exception.ErrorCode.ALREADY_EXISTS_NAME;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserResDto create(SignUpDto dto) {
+        User user = SignUpDto.toEntity(dto);
+
+        Optional<User> foundUserByName = userRepository.findByName(user.getName());
+        if(foundUserByName.isPresent())
+            throw new CustomException(ALREADY_EXISTS_NAME);
+
+        Optional<User> foundUserByEmail = userRepository.findByEmail(user.getEmail());
+        if(foundUserByEmail.isPresent())
+            throw new CustomException(ALREADY_EXISTS_EMAIL);
+
+        //TODO: 비밀번호 암호화
+        User newUser = userRepository.save(user);
+
+        return new UserResDto(newUser);
+    }
 }
